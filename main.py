c4cd556993aa591aa267273171b21b4680935d34
Freddy  Cruz Chinguel <FRED_16_09@hotmail.com>
2023-03-07 12:54:14 -0500

    Agregando los 826 personajes

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import requests as requests
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+# MONGO_URI = "mongodb://localhost"
+# client = MongoClient(MONGO_URI)
+# db = client['RickAndMortyAPI']
+# collection = db['personajes']
+
+# for pagina in range(1, 21):
+#     peticion = requests.get(f"https://rickandmortyapi.com/api/character?page={pagina}")
+#     datos = peticion.json()
+#     #collection.insert_many(datos["results"])
+    
+    
 MONGO_URI = "mongodb://localhost"
 client = MongoClient(MONGO_URI)
 db = client['RickAndMortyAPI']
 collection = db['personajes']
 
-for pagina in range(1, 21):
+for pagina in range(1, 43):
     peticion = requests.get(f"https://rickandmortyapi.com/api/character?page={pagina}")
     datos = peticion.json()
     #collection.insert_many(datos["results"])
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def examen():
-#     todos = collection.aggregate([{ "$sort" : { "id" : -1 } }])
-#     return render_template("index.html", lista = todos)
 
 @app.route("/")
 @app.route("/<int:pagina>")
